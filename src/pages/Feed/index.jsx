15c151f2ed3943fb3282fcda2b18728de5180176
jojosuelobo <josuelobo2000@gmail.com>
2023-09-10 22:07:17 -0300

    Carregamento de post via GET

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,13 +1,62 @@
+// Estilos
+import styles from './Feed.module.sass'
+
+// Componentes 
 import Aside from '../../components/asideCustom'
 import Header from '../../components/header'
 
-export default function feed() {
+// 
+import blogFetch from '../../axios/config'
+import { useState, useEffect } from 'react'
+
+export default function Feed() {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/posts")
+      const data = response.data
+      setPosts(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <>
       <Header />
-      <section>
+      <section className={styles.main}>
+        {/* Menu a esquerda dá página */}
         <Aside />
-        feed
+
+        {/* Conteúdo principal da página */}
+        {posts.length === 0 ? (<p>Carregando...</p>) : (
+          posts.map((post) => (
+            <div className={styles.post} key={post.data_postagem}>
+              <h2 className={styles.title}>{post.titulo}</h2>
+              <p className={styles.date}>{post.data_postagem}</p>
+
+              <div className={styles.tags}>
+                {(post.tags_relacionadas).map((tag) => (
+                  <p className={styles.tag} key={tag}>{tag}</p>
+                ))}
+              </div>
+
+              <p className={styles.desc}>{post.descricao}</p>
+              <div className={styles.post_footer}>
+                <img className={styles.profile_pic} src="" alt="" />
+                <p className={styles.username}>{post.nome_usuario}</p>
+                <button>Abrir</button>
+              </div>
+            </div>
+          ))
+        )}
+
+
       </section>
     </>
   )
