c3fb25d57c472d4818a2a5911ad41258ea8e4063
Eduardo <eduardo.sorrentino5@gmail.com>
2023-09-17 23:08:09 -0300

    adding responsiveness

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,32 +2,31 @@ import logo from '../../../public/logoUVV.png'
 import styles from './Login.module.sass'
 import { Link } from 'react-router-dom';
 
-
 export default function Login() {
   return (
     <section className={styles.main_content}> 
-            <div className={styles.login_page}>
-                <img className={styles.logo} src={logo} alt="logo" />
-                <div className={styles.text}>
-                    <h1 className={styles.title}>Welcome back!</h1>
-                    <p>Please enter your details</p>
-                </div>
-                <form className={styles.login_form}>
-                    <p>Usuário ou Email</p>
-                    <input
-                        className={styles.login}
-                        type="text"
-                    />
-                    <p>Senha</p>
-                    <input
-                        className={styles.password}
-                        type="password"
-                    />
-                    <p>Esqueceu sua senha?</p>
-                </form>
-                <button className={styles.btn}>Entrar</button>
-                <p className={styles.redirect_login}>Não tem uma conta ainda? <Link to='/register'><a>Criar conta</a></Link></p>
-            </div>
-        </section>
+      <div className={styles.login_page}>
+        <img className={styles.logo} src={logo} alt="logo" />
+        <div className={styles.login_text}>
+          <h1 className={styles.title}>Welcome back!</h1>
+          <p>Please enter your details</p>
+        </div>
+        <form className={styles.login_form}>
+          <p>Usuário ou Email</p>
+          <input
+            className={styles.login}
+            type="text"
+          />
+          <p>Senha</p>
+          <input
+            className={styles.password}
+            type="password"
+          />
+                  <p style={{ fontSize: '0.775rem', textAlign: 'right', marginTop: '7px', cursor: 'pointer' }}>Esqueceu sua senha?</p>
+        </form>
+        <button className={styles.btn}>Entrar</button>
+        <p className={styles.redirect_login} style={{ marginTop: '-15px', fontSize: '0.875rem' }}>Não tem uma conta ainda? <Link to='/register'><a>Criar conta</a></Link></p>
+      </div>
+    </section>
   )
 }
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -4,33 +4,27 @@ import { Link } from 'react-router-dom';
 
 export default function Register() {
     return (
-        <section className={styles.main_content}> 
+        <section className={styles.main_content}>
             <div className={styles.register_page}>
                 <img className={styles.logo} src={logo} alt="logo" />
                 <div className={styles.register_text}>
-                    <h1>Join to Company Name </h1>
+                    <h1>Join to Company Name</h1>
                     <p>Company details here</p>
                 </div>
                 <form className={styles.register_form}>
                     <p>Nome</p>
-                    <input
-                        type="text"
-                    />
+                    <input type="text" />
                     <p>Email</p>
-                    <input
-                        type="text"
-                    />
+                    <input type="text" />
                     <p>Senha</p>
-                    <input
-                        type="password"
-                    />
+                    <input type="password" />
                     <p>Confirmar Senha</p>
-                    <input
-                        type="password"
-                    />
+                    <input type="password" />
                 </form>
                 <button className={styles.btn}>Criar conta</button>
-                <p className={styles.redirect_login}>Já possui conta? <Link to='/login'><a>Entrar</a></Link></p>
+                <p className={styles.redirect_login}>
+                    Já possui conta? <Link to='/login'><a>Entrar</a></Link>
+                </p>
             </div>
         </section>
     )
