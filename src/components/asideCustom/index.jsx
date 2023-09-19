b9e878fa0228cc7e0007645bf56bcdb1c937c230
MatheusCSouza30 <141576540+MatheusCSouza30@users.noreply.github.com>
2023-09-19 20:30:42 -0300

    Update index.jsx

--- src/components/asideCustom/index.jsx
+++ src/components/asideCustom/index.jsx
@@ -5,13 +5,16 @@ export default function aside() {
     <aside className={styles.main_content}>
       <ul>
         <li>
-          <h1>home</h1>
+          <button className={styles.btn}><img src="inicial.png" width="30" height="30" align="left"/><h1>Página Inicial</h1></button>
         </li>
         <li>
-          <h1>feed</h1>
+          <button className={styles.btn}><img src="notificacao.png" width="30" height="30" align="left" /><h1>Notificações</h1></button>
         </li>
         <li>
-          <h1>etc</h1>
+          <button className={styles.btn}><img src="perfil.png" width="30" height="30" align="left" /><h1>Perfil</h1> </button>
+        </li>
+        <li>
+          <button className={styles.btn2}><h1>Criar Lista</h1></button>
         </li>
       </ul>
     </aside>
