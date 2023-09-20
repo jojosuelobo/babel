3be10f5320b2f1989c47a8bf90c7c17fd3510455
jojosuelobo <josue.lobo@etaure.com.br>
2023-09-20 12:38:39 -0300

    refatoração para o componente de busca

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -9,11 +9,9 @@ import PostDetail from '../../components/postDetail'
 // 
 import blogFetch from '../../axios/config'
 import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Feed() {
-  const navigate = useNavigate()
-  const [query, setQuery] = useState("");
   const [posts, setPosts] = useState([])
 
   const getPosts = async () => {
@@ -26,11 +24,6 @@ export default function Feed() {
     }
   }
 
-  const handleSearch = async () => {
-    if(query.trim() === '')
-      return
-    return navigate(`/search?q=${query.trim()}`)
-  }
 
   useEffect(() => {
     getPosts()
@@ -42,16 +35,6 @@ export default function Feed() {
       <section className={styles.main}>
         {/* Menu a esquerda dá página */}
         <Aside />
-
-        <div className={styles.search_container}>
-          <input
-            type="text"
-            placeholder="Buscar"
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button onClick={handleSearch}>Pesquisar</button>
-        </div>
-
         {/* Conteúdo principal da página */}
         <div className={styles.feed}>
           {posts.length === 0 ? (
