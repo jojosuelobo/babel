fadfe0a81fe9e162a5b349222c5b434fd72ed88f
jojosuelobo <josue.lobo@etaure.com.br>
2023-10-03 14:35:33 -0300

    ajustes de clean code

--- src/pages/NewPost/index.jsx
+++ src/pages/NewPost/index.jsx
@@ -26,7 +26,6 @@ export default function NewPost() {
 
     const [titulo, setTitulo] = useState('')
     const dataPostagem = moment().format('L')
-    const [tagInput, setTagInput] = useState('')
     const [tag, setTags] = useState([])
     const [descricao, setDescricao] = useState('')
 
@@ -39,18 +38,15 @@ export default function NewPost() {
     const displayName = user.displayName
 
     // Listagem de fato
-    const [quantidadeDeItensLista, setQuantidadeDeItensLista] = useState(2)
     const [lista, setLista] = useState([
         {
-            nome_item: 'teste',
-            descricao_item: 'TESTE'
+            nome_item: '',
+            descricao_item: ''
         }
     ]);
 
     const handleSoma = () => {
-        //setQuantidadeDeItensLista(quantidadeDeItensLista + 1)
-        let novoItem = {nome_item: 'testeAdição', descricao_item: 'TESTEadição'}
-        //let novaLista = {...lista, novoItem}
+        let novoItem = {nome_item: '', descricao_item: 'TESTEadição'}
         setLista([...lista, novoItem])
     }
 
@@ -61,38 +57,11 @@ export default function NewPost() {
         }
     }
 
-    // Atualiza a lista sempre que quantidadeDeItensLista mudar
-
-    // useEffect(() => {
-    //     const novaLista = Array.from({ length: quantidadeDeItensLista }, () => ({
-    //         nome_item: 'teste',
-    //         descricao_item: 'TESTE'
-    //     }));
-    //     setLista(novaLista);
-    // }, [quantidadeDeItensLista]);
-
-    // const [lista, setLista] = useState([
-    //     {
-    //         nome_item: '',
-    //         descricao_item: ''
-    //     },
-    //     {
-    //         nome_item: '',
-    //         descricao_item: ''
-    //     },
-    //     {
-    //         nome_item: '',
-    //         descricao_item: ''
-    //     }
-    // ])
-
-
     const handleSubmit = (e) => {
         e.preventDefault();
 
         // ID
         const idPost = Math.floor(Math.random() * 1000)
-
 
         const post = {
             id: idPost,
@@ -102,36 +71,13 @@ export default function NewPost() {
             descricao,
             nome_usuario: displayName,
             itens_lista: lista
-            // itens_lista: [
-            //     {
-            //         nome_item: "One Piece",
-            //         descricao_item: "Anime de pirata que estica"
-            //     },
-            //     {
-            //         nome_item: "Naruto",
-            //         descricao_item: "Alguma coisa sobre ninjas"
-            //     },
-            //     {
-            //         nome_item: "Fluminense",
-            //         descricao_item: "Salve o corintianssssss o campeão dos campeoesssssssssss"
-            //     },
-            // ]
         }
 
         console.log(post)
         httpConfig(post, "POST")
 
-        // Clear dos campos
+        // Redirecionar
     }
-
-
-    const preencher = () => {
-        setQuantidadeDeItensLista(3)
-        setTitulo('Melhores receitas com morango')
-        //setTags((('receitas,culinária,sobremesa').split(",").map((tag) => tag.trim())).filter((tag) => tag !== ""))
-        setDescricao('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis massa sem, fermentum eu egestas in, luctus vitae nibh. Mauris viverra erat velit, nec suscipit nunc finibus consectetur. Nam quis malesuada nisl. Suspendisse at velit id ex semper convallis. Aliquam efficitur leo sit amet gravida dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis non feugiat quam. In tincidunt ullamcorper felis, nec elementum nisl ultrices at. Proin auctor sagittis tortor, at elementum ante venenatis id. Nam convallis, enim eu viverra lacinia, enim sapien eleifend purus, sit amet imperdiet orci arcu at enim. Fusce vestibulum tellus sit amet augue ultrices cursus. Donec eget lorem bibendum, cursus urna congue, feugiat neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.')
-    }
-
     return (
         <>
             <Header />
@@ -140,7 +86,6 @@ export default function NewPost() {
                     <div className={styles.header}>
                         <Link to={'/'}> <IoMdArrowRoundBack className={styles.icon} /></Link>
                         <h1>Nova lista</h1>
-                        <button onClick={preencher}>auto preencher</button>
                     </div>
                     <form className={styles.form} onSubmit={handleSubmit}>
                         <label className={styles.title}>
@@ -167,9 +112,6 @@ export default function NewPost() {
                         </label>
 
                         <div className={styles.itens}>
-
-                            {/* lista.map((item, index) => (<h1 key={index}>oi</h1>)) */}
-
                             {lista.map((item, index) => (
                                 <div className={styles.item_lista} key={index}>
                                     <label className={styles.item_tit}>
@@ -198,45 +140,14 @@ export default function NewPost() {
                                     </label>
                                 </div>
                             ))} 
-
-                            {/* {Array.from({ length: quantidadeDeItensLista }, (_, index) => (
-                                <div className={styles.item_lista} key={index}>
-                                    <label className={styles.item_tit}>
-                                        Título {index + 1}
-                                        <input
-                                            type="text"
-                                            className={styles.item_input}
-                                            onChange={(e) => {
-                                                const newList = [...lista];
-                                                newList[index].nome_item = e.target.value;
-                                                setLista(newList);
-                                            }}
-                                        />
-                                    </label>
-                                    <label className={styles.descricao}>
-                                        Descrição
-                                        <textarea
-                                            type="text"
-                                            className={styles.item_text}
-                                            onChange={(e) => {
-                                                const newList = [...lista];
-                                                newList[index].descricao_item = e.target.value;
-                                                setLista(newList);
-                                            }}
-                                        ></textarea>
-                                    </label>
-                                </div>
-                                        ))} */}
                         </div>
-
                         <div className={styles.botoes}>
-
                             {loading ? <p>Aguarde!</p>
                                 : <input className={styles.submit} type="submit" value="Criar" />}
                         </div>
                     </form>
-                    <button onClick={() => setQuantidadeDeItensLista(handleSoma)} className={styles.submit}>Adicionar item</button>
-                    <button onClick={() => setQuantidadeDeItensLista(handleRemove)} className={styles.submit}>Remover item</button>
+                    <button onClick={handleSoma} className={styles.submit}>Adicionar item</button>
+                    <button onClick={handleRemove} className={styles.submit}>Remover item</button>
                 </div>
 
             </div>
